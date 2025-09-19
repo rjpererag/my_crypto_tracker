ba84184e22a98265de054a2c3a314a01f54b0a02
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2025-09-19 12:19:43 +0200

    fix/updated makefile and schema queries

--- backend/database/schema/schema.sql
+++ backend/database/schema/schema.sql
@@ -1,23 +1,20 @@
 CREATE extension if NOT EXISTS "uuid-ossp";
 
-DROP TABLE if EXISTS symbols CASCADE;
-CREATE table symbols(
+CREATE table IF NOT EXISTS symbols(
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 	name text NOT NULL,
 	symbol text NOT NULL
 );
 
 
-DROP TABLE if EXISTS exchange CASCADE;
-CREATE TABLE exchange(
+CREATE TABLE IF NOT EXISTS exchange(
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 	name text NOT NULL,
 	country text
 );
 
 
-DROP TABLE if EXISTS tickers CASCADE;
-CREATE TABLE tickers(
+CREATE TABLE IF NOT EXISTS tickers(
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 	symbols_id UUID NOT NULL,
 	exchange_id UUID NOT NULL,
@@ -28,8 +25,7 @@ CREATE TABLE tickers(
 );
 
 
-DROP TABLE if EXISTS prices CASCADE;
-CREATE TABLE prices(
+CREATE TABLE IF NOT EXISTS prices(
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 	ticker_id UUID NOT NULL,
 	price NUMERIC NOT NULL,
